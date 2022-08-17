aa03faff8f73f39ed00c7b7e8de917a5b5832e2c
Yu-Wei Chang <andy.yu29@gmail.com>
2022-08-17 20:09:32 +0800

    Add kinova gripper2 model

--- include/kinova_test/KinovaGen3Model.h
+++ include/kinova_test/KinovaGen3Model.h
@@ -27,6 +27,13 @@ void kinova_G_gripper(double g, double q1, double q2, double q3,
                       double q4, double q5, double q6, double q7,
                       double G[7]);
 
+void kinova_M_gripper2(double q1, double q2, double q3, double q4,
+                       double q5, double q6, double q7, double M[49]);
+
+void kinova_C_gripper2(double q1, double q2, double q3, double q4,
+                       double q5, double q6, double q7, double dq1,
+                       double dq2, double dq3, double dq4, double dq5,
+                       double dq6, double dq7, double C[49]);
 // Parameters
 enum class Params
 {
